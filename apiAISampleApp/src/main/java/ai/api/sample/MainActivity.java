74db4207d97d767528d61e0c51ebdb85879b6bf1
Danil Skachkov <danil.skachkov@speaktoit.com>
2014-09-10 12:42:35 +0700

    Add example code about using AI service response object

--- apiAISampleApp/src/main/java/ai/api/sample/MainActivity.java
+++ apiAISampleApp/src/main/java/ai/api/sample/MainActivity.java
@@ -21,8 +21,9 @@ package ai.api.sample;
  *
  ***********************************************************************************************************************/
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -31,6 +32,10 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+
+import java.util.Locale;
+import java.util.Map;
 
 import ai.api.AIConfiguration;
 import ai.api.AIListener;
@@ -39,10 +44,10 @@ import ai.api.GsonFactory;
 import ai.api.model.AIError;
 import ai.api.model.AIResponse;
 
-import java.util.Locale;
-
 
 public class MainActivity extends ActionBarActivity implements AIListener {
+
+    public static final String TAG = MainActivity.class.getName();
 
     private static final String ACCESS_TOKEN = "INSERT_CLIENT_ACCESS_TOKEN_HERE";
     private AIService aiService;
@@ -105,11 +110,38 @@ public class MainActivity extends ActionBarActivity implements AIListener {
     }
 
     @Override
-    public void onResult(final AIResponse result) {
-        if (result.isError()) {
-            resultTextView.setText("Error: " + result.getStatus().getErrorDetails());
+    public void onResult(final AIResponse response) {
+        if (response.isError()) {
+            resultTextView.setText("Error: " + response.getStatus().getErrorDetails());
+
+            Log.i(TAG, "Received error response");
+
+            // this is example how to get different parts of error description
+            Log.i(TAG, "Error details: " + response.getStatus().getErrorDetails());
+            Log.i(TAG, "Error type: " + response.getStatus().getErrorType());
         } else {
-            resultTextView.setText(gson.toJson(result));
+            resultTextView.setText(gson.toJson(response));
+
+            Log.i(TAG, "Received success response");
+
+            // this is example how to get different parts of result object
+            Log.i(TAG, "Status: " + response.getStatus());
+            Log.i(TAG, "Resolved query: " + response.getResolvedQuery());
+
+            Log.i(TAG, "Action: " + response.getResult().getAction());
+            Log.i(TAG, "Speech: " + response.getResult().getSpeech());
+
+            if (response.getResult().getMetadata() != null) {
+                Log.i(TAG, "Intent id: " + response.getResult().getMetadata().getIntentId());
+                Log.i(TAG, "Intent name: " + response.getResult().getMetadata().getIntentName());
+            }
+
+            if (response.getResult().getParameters() != null && !response.getResult().getParameters().isEmpty()) {
+                Log.i(TAG, "Parameters: ");
+                for (final Map.Entry<String, JsonElement> entry : response.getResult().getParameters().entrySet()){
+                    Log.i(TAG, String.format("%s: %s", entry.getKey(), entry.getValue().toString()));
+                }
+            }
         }
     }
 
